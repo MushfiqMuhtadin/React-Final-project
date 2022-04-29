b62be117166b32b1dce9e5d881c650477349da1a
MushfiqMuhtadin <mushfiq1519@gmail.com>
2022-04-29 16:25:26 +0600

    react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './Header'
+
+import { BrowserRouter as Router, Routes, Route, Link, BrowserRouter } from "react-router-dom";
+import Login from './Login'
+import Register from './Register';
+import AddProduct from './AddProduct';
+import UpdateProduct from './UpdateProduct';
+import Admin from './Admin';
+import Usercrud from './usercrud';
+import Productcrud from './Productcrud';
+import Footer from './footer';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="">
+        <BrowserRouter>
+          {/* <Header /> */}
+          <Routes>
+            <Route path="/" element={<Login />}>
+
+            </Route>
+            <Route path="/add" element={<AddProduct />}>
+
+            </Route>
+            <Route path="/update" element={<UpdateProduct />}>
+
+            </Route>
+            <Route path="/login" element={<Login />}>
+
+            </Route>
+            <Route path="/register" element={<Register />}>
+              
+            </Route>
+
+            <Route path="/admin" element={<Admin />}>
+              
+            </Route>
+
+            <Route path="/usercrud" element={<Usercrud />}>
+              
+            </Route>
+
+            <Route path="/Productcrud" element={<Productcrud />}>
+              
+            </Route>
+
+
+          </Routes>
+        </BrowserRouter>
+      </div>
     </div>
   );
 }
